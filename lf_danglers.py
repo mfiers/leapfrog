d7280d4a87edbe8b508f36de515740c60ebc892a
Tim Millar <tim.millar@plantandfood.co.nz>
2016-02-15 15:49:35 +1300

    Basic pep8 corrections

--- lf_danglers.py
+++ lf_danglers.py
@@ -16,39 +16,37 @@ parser.add_argument('-p', '--bowtie_preset', default='fast')
 
 args = parser.parse_args()
 
+
 def run_bowtie2(db, fq):
     cl = "bowtie2 --%s --reorder --no-head --quiet -p %d -x %s -U %s" % (
         args.bowtie_preset, args.threads, db, fq)
     process = sp.Popen(cl, shell=True, stdout=sp.PIPE)
     for line in process.stdout:
-        if line[0] == '@': continue
+        if line[0] == '@':
+            continue
         yield line.split()
 
-i = 0; j = 1
-with open(args.output, 'w') as F:
-    for forward, reverse in itertools.izip(
-        run_bowtie2(args.bowtie2_database, args.forward_reads),
-        run_bowtie2(args.bowtie2_database, args.reverse_reads)):
 
+i = 0
+j = 1
+with open(args.output, 'w') as F:
+    for forward, reverse in itertools.izip(run_bowtie2(args.bowtie2_database, args.forward_reads), run_bowtie2(args.bowtie2_database, args.reverse_reads)):
         i += 1
         fflag = int(forward[1])
         rflag = int(reverse[1])
         if fflag & 0x4 == rflag & 0x4:
-            #either both map, or both do not map
-            #not dangling
+            # either both map, or both do not map
+            # not dangling
             continue
 
         if fflag & 0x4:
-            #forward is dangling        
-            tag = 'R' + {0:'+', 0x10:'-'}[rflag & 0x10]
+            # forward is dangling
+            tag = 'R' + {0: '+', 0x10: '-'}[rflag & 0x10]
             F.write('@%s__%s__%s\n' % (reverse[2], tag, forward[0]))
             F.write('%s\n+\n%s\n' % (forward[9], forward[10]))
 
         else:
-            #reverse is dangling
-            tag = 'F' + {0:'+', 0x10:'-'}[fflag & 0x10]
+            # reverse is dangling
+            tag = 'F' + {0: '+', 0x10: '-'}[fflag & 0x10]
             F.write('@%s__%s__%s\n' % (forward[2], tag, reverse[0]))
             F.write('%s\n+\n%s\n' % (reverse[9], reverse[10]))
-
-
- 
