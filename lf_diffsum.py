188a560f9934c8bd42f8faa28c69665e5425b168
Tim Millar <tim.millar@plantandfood.co.nz>
2016-02-15 16:19:19 +1300

    White space fixes

--- lf_diffsum.py
+++ lf_diffsum.py
@@ -4,7 +4,7 @@ import sys
 import collections
 
 counts = collections.defaultdict(list)
-total = 0        
+total = 0
 simmat = collections.defaultdict(int)
 libs = set()
 with open(sys.argv[1]) as F:
@@ -19,7 +19,7 @@ with open(sys.argv[1]) as F:
         for a in sets:
             libs.add(a)
             for b in sets:
-                simmat[(a,b)] += 1
+                simmat[(a, b)] += 1
 
 
 print 'total', total
@@ -28,28 +28,28 @@ libs = sorted(libs)
 base = sys.argv[2]
 
 with open(base + '.venn', 'w') as F:
-    kys = sorted([(len(x), x ) for x in counts.keys()])
+    kys = sorted([(len(x), x) for x in counts.keys()])
     for ln, s in kys:
         F.write("%s\t%s\n" % (len(counts[s]), ",".join(s[1:])))
 
 with open(base + '.sim', 'w') as F:
-    for l in libs: 
+    for l in libs:
         F.write("\t%s" % l)
     F.write("\n")
 
     for l in libs:
         F.write("%s" % l)
         for m in libs:
-            F.write("\t%s" % simmat[(l,m)])
+            F.write("\t%s" % simmat[(l, m)])
         F.write("\n")
 
 with open(base + '.simf', 'w') as F:
-    for l in libs: 
+    for l in libs:
         F.write("\t%s" % l)
     F.write("\n")
 
     for l in libs:
         F.write("%s" % l)
         for m in libs:
-            F.write("\t%f" % (float(simmat[(l,m)])/total))
+            F.write("\t%f" % (float(simmat[(l, m)])/total))
         F.write("\n")
