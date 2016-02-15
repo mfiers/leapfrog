630f10ad1bcce85bd14e37691e58e781eaa31224
Tim Millar <tim.millar@plantandfood.co.nz>
2016-02-15 15:28:01 +1300

    Removed import statements for unused modules

--- lf_finddiff.py
+++ lf_finddiff.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import string
 import argparse
 import collections
--- lf_regionify.py
+++ lf_regionify.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 import numpy as np
-
 import pysam
-
 import logging
 import argparse
 import collections
