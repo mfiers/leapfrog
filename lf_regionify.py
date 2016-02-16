f95954740880b4fdd0741e30a424e009115d7f56
Tim Millar <tim.millar@plantandfood.co.nz>
2016-02-16 14:10:11 +1300

    Formatting parseer args help string in regionify.py

--- lf_regionify.py
+++ lf_regionify.py
@@ -9,66 +9,54 @@ import collections
 logging.basicConfig()
 lg = logging.getLogger(__name__)
 
-md_help = " ".join("""
-Stringency to determine if a hit maps uniquely. This score is the
-minimal allowed difference between the scores of the first and second
-hit before a read is assumed to be mapped to it's correct, unique,
-location. (default=5)
-""".split())
-
-mincov_help = " ".join("""
-Minimal coverage for a family to be exported as a peak - the number
-checked is the maximal coverage for that set of mapping
-reads. (default 4)
-""".split())
-
-peakfilter_help = " ".join("""
-Minimal coverage of a read region that is considered part of the
-peak. This number is used to trim trailing edges from a peak, or, if
-necessary to cut a peak into an number of smaller peaks. Note, if you
-set a value below 0, it will be interpreted as a fraction of the
-maximal coverage observed for that peak. (default: 3)
-""".split())
-
-
-mintrimcov_help = " ".join("""
-When using a fraction for -c, this value sets a lower limit - anything
-below this value will be trimmed for sure. (default 3)
-""").split()
-
 parser = argparse.ArgumentParser('Identify transposon flanking regions')
-
 parser.add_argument('input_bam')
-
 parser.add_argument('-a',
                     '--min_coverage',
                     type=int, default=4,
-                    help=mincov_help)
-
+                    help=("Minimal coverage for a family to be "
+                          "exported as a peak - the number "
+                          "checked is the maximal coverage for "
+                          "that set of mapping reads. (default 4)"))
 parser.add_argument('-m',
                     '--min_diff',
                     type=int,
                     default=5,
-                    help=md_help)
-
+                    help=("Stringency to determine if a hit maps "
+                          "uniquely. This score is the "
+                          "minimal allowed difference between the "
+                          "scores of the first and second "
+                          "hit before a read is assumed to be "
+                          "mapped to it's correct, unique, "
+                          "location. (default=5)"))
 parser.add_argument('-c',
                     '--trim_cov',
                     type=float,
                     default=3.,
-                    help=peakfilter_help)
-
+                    help=("Minimal coverage of a read region "
+                          "that is considered part of the "
+                          "peak. This number is used to trim "
+                          "trailing edges from a peak, or, if "
+                          "necessary to cut a peak into an "
+                          "number of smaller peaks. Note, if you "
+                          "set a value below 0, it will be "
+                          "interpreted as a fraction of the "
+                          "maximal coverage observed for that "
+                          "peak. (default: 3)"))
 parser.add_argument('-C',
                     '--min_trim_cov',
                     type=int,
                     default=3,
-                    help=mintrimcov_help)
-
+                    help=("When using a fraction for -c, this "
+                          "value sets a lower limit - anything "
+                          "below this value will be trimmed "
+                          "for sure. (default 3)"))
 parser.add_argument('-q',
                     '--output_nonunique',
                     action='store_true',
                     default=False,
-                    help='also output peaks which are no likely to be uniquely mapped')
-
+                    help=("also output peaks which are no likely "
+                          "to be uniquely mapped"))
 args = parser.parse_args()
 
 sam = pysam.Samfile(args.input_bam, 'rb')
