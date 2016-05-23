beca5e7c8431be3b4b6144bf177b4003c1238eab
Tim Millar <tim.millar@plantandfood.co.nz>
2016-05-23 12:27:30 +1200

    Commented out unused cli parameters

--- lf_regionify2.py
+++ lf_regionify2.py
@@ -11,11 +11,11 @@ from sklearn.cluster import DBSCAN
 def parse_args(args):
     parser = argparse.ArgumentParser('Identify transposon flanking regions')
     parser.add_argument('input_bam')
-    parser.add_argument('-a', '--min_coverage', type=int, default=4,
-                        help=("Minimal coverage for a family to be "
-                              "exported as a peak - the number "
-                              "checked is the maximal coverage for "
-                              "that set of mapping reads. (default 4)"))
+#    parser.add_argument('-a', '--min_coverage', type=int, default=4,
+#                        help=("Minimal coverage for a family to be "
+#                              "exported as a peak - the number "
+#                              "checked is the maximal coverage for "
+#                              "that set of mapping reads. (default 4)"))
     parser.add_argument('-m', '--min_diff', type=int, default=5,
                         help=("Stringency to determine if a hit maps "
                               "uniquely. This score is the "
@@ -24,26 +24,26 @@ def parse_args(args):
                               "hit before a read is assumed to be "
                               "mapped to it's correct, unique, "
                               "location. (default=5)"))
-    parser.add_argument('-c', '--trim_cov', type=float, default=3.,
-                        help=("Minimal coverage of a read region "
-                              "that is considered part of the "
-                              "peak. This number is used to trim "
-                              "trailing edges from a peak, or, if "
-                              "necessary to cut a peak into an "
-                              "number of smaller peaks. Note, if you "
-                              "set a value below 0, it will be "
-                              "interpreted as a fraction of the "
-                              "maximal coverage observed for that "
-                              "peak. (default: 3)"))
-    parser.add_argument('-C', '--min_trim_cov', type=int, default=3,
-                        help=("When using a fraction for -c, this "
-                              "value sets a lower limit - anything "
-                              "below this value will be trimmed "
-                              "for sure. (default 3)"))
-    parser.add_argument('-q', '--output_nonunique',
-                        action='store_true', default=False,
-                        help=("also output peaks which are no likely "
-                              "to be uniquely mapped"))
+#    parser.add_argument('-c', '--trim_cov', type=float, default=3.,
+#                        help=("Minimal coverage of a read region "
+#                              "that is considered part of the "
+#                              "peak. This number is used to trim "
+#                              "trailing edges from a peak, or, if "
+#                              "necessary to cut a peak into an "
+#                              "number of smaller peaks. Note, if you "
+#                              "set a value below 0, it will be "
+#                              "interpreted as a fraction of the "
+#                              "maximal coverage observed for that "
+#                              "peak. (default: 3)"))
+#    parser.add_argument('-C', '--min_trim_cov', type=int, default=3,
+#                        help=("When using a fraction for -c, this "
+#                              "value sets a lower limit - anything "
+#                              "below this value will be trimmed "
+#                              "for sure. (default 3)"))
+#    parser.add_argument('-q', '--output_nonunique',
+#                        action='store_true', default=False,
+#                        help=("also output peaks which are no likely "
+#                              "to be uniquely mapped"))
     parser.add_argument('--eps', type=int, default=100,
                         help=("When using the DBSCAN method to identify "
                               "read clusters, eps is the minimum distance "
